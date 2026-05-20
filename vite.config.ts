654d5bee0f9bf3b9931168f51628bb07eb721556
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-05-20 11:31:41 +0200

    chore: switch to adapter-static and add self-referencing alias for dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      "@computational-biology-aachen/design": resolve("./src/lib/"),
+      "@computational-biology-aachen/design/tokens.css": resolve(
+        "./src/lib/tokens.css",
+      ),
+    },
+  },
 });
