a1f01f950db4fa8943470cc8378e8afd4acf54cc
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-05-28 10:09:01 +0200

    fix: url config

---
+++ src/lib/config.ts
@@ -0,0 +1,7 @@
+import { dev } from "$app/environment";
+
+export const title = "mxlweb";
+export const description = "CPBL design";
+export const url = dev
+  ? "http://localhost:5173/"
+  : "https://https://computational-biology-aachen.github.io/design/";
