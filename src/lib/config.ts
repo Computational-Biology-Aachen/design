86ed153fbc86088e45876bd42ff27f9380094187
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-06-05 10:24:37 +0200

    fix: unify config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,4 +4,4 @@ export const title = "mxlweb";
 export const description = "CPBL design";
 export const url = dev
   ? "http://localhost:5173/"
-  : "https://https://computational-biology-aachen.github.io/design/";
+  : "https://computational-biology-aachen.github.io/design/";
